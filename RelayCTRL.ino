eaadb1bf09bfacf7ef82edbaf99f55b168045293
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-03-29 02:16:02 +0900

    Final Version

---
+++ RelayCTRL.ino
@@ -0,0 +1,65 @@
+#include <CapacitiveSensor.h>
+#include <SoftwareSerial.h>
+
+CapacitiveSensor cs_4_7 = CapacitiveSensor(4, 7);
+
+int tx = 2;
+int rx = 3;
+int relay = 10;
+boolean RLY;
+
+SoftwareSerial BT(tx, rx);
+String cmd = "";
+
+void setup()
+{
+  BT.begin(9600);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+  RLY = false;
+}
+
+void loop()
+{
+  while(BT.available())
+  {
+    char RCV = (char)BT.read();
+    cmd += RCV;
+    delay(5);
+  }
+  if(!cmd.equals(""))
+  {
+    if(cmd == "sw")
+    {
+      if(RLY == true)
+      {
+        digitalWrite(relay, HIGH);
+        RLY = false;
+      }
+      else if(RLY == false)
+      {
+        digitalWrite(relay, LOW);
+        RLY = true;
+      }
+    }
+    cmd="";
+  }
+
+  long TCH = cs_4_7.capacitiveSensorRaw(30);
+  if(TCH > 1000)
+  {
+    if(RLY == true)
+      {
+        digitalWrite(relay, HIGH);
+        RLY = false;
+        delay(200);
+      }
+      else if(RLY == false)
+      {
+        digitalWrite(relay, LOW);
+        RLY = true;
+        delay(200);
+      }
+  }
+  delay(50);
+}
---
+++ Relay_CTRL_WIFI.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor CS = CapacitiveSensor(4, 5);
+
+char auth[] = "b5e348e34fb44bdf82cf611a8b06567e";
+char ssid[] = "Luft Aquila";
+char pass[] = "ljhobjokr";
+
+boolean RLY;
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  RLY = false;
+
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  
+  long TCH = CS.capacitiveSensorRaw(30);
+  if(TCH > 10000)
+  {
+    if(RLY == true)
+    {
+      digitalWrite(13, LOW);
+      Blynk.virtualWrite(V0, 0);
+      RLY = false;
+      delay(200);
+    }
+    else if(RLY == false)
+    {
+      digitalWrite(13, HIGH);
+      Blynk.virtualWrite(V0, 1);
+      RLY = true;
+      delay(200);
+    }
+  }
+}
+
+BLYNK_WRITE(V0)
+{
+  int rcv = param.asInt();
+  if(rcv == 1)
+  {
+    digitalWrite(13, HIGH);
+    RLY = true;
+  }
+  else if(rcv == 0)
+  {
+    digitalWrite(13, LOW);
+    RLY = false;
+  }
+}
+
+BLYNK_CONNECTED()
+{
+  Blynk.syncVirtual(V0);
+}
+
+BLYNK_APP_CONNECTED()
+{
+  if(RLY == true)
+    Blynk.virtualWrite(V0, 1);
+  else if(RLY == false)
+    Blynk.virtualWrite(V0, 0);
+}
